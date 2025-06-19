39ec50a0b786d6f6261db321abd927d4d934f37e
Zulqarnain04 <147962196+Zulqarnain04@users.noreply.github.com>
2025-06-19 22:18:17 +0800

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,1 +1,28 @@
+//user navigation based of role
 
+<?php
+if (!isset($_SESSION)) session_start();
+?>
+<nav>
+  <ul>
+    <li><a href="/dashboard.php">Dashboard</a></li>
+    
+    //student's nav
+    <?php if ($_SESSION['role'] == 'student'): ?>
+      <li><a href="/student/apply.php">Apply</a></li>
+      <li><a href="/student/status.php">Status</a></li>
+    
+    //manager nav
+    <?php elseif ($_SESSION['role'] == 'manager'): ?>
+      <li><a href="/manager/review_applications.php">Review</a></li>
+      <li><a href="/manager/sort_reports.php">Reports</a></li>
+
+    //admin nav
+    <?php elseif ($_SESSION['role'] == 'admin'): ?>
+      <li><a href="/admin/manage_users.php">Manage Users</a></li>
+    <?php endif; ?>
+
+    //logout 
+    <li><a href="/logout.php">Logout</a></li>
+  </ul>
+</nav>
