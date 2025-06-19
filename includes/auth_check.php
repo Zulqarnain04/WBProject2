feb4fa696be497f32c688921de3574c908c35c23
Zulqarnain04 <147962196+Zulqarnain04@users.noreply.github.com>
2025-06-19 22:20:51 +0800

    Update auth_check.php

--- includes/auth_check.php
+++ includes/auth_check.php
@@ -1,1 +1,10 @@
+//check if user login . put at every page that need login
 
+<?php
+session_start();
+
+//if not login redirect to login page
+if(!isset($_SESSION['username'])) {
+  header("Location: /login.php");
+  exit();
+}
