b54b5ac798cfcf83fd50dea52e37274579909b46
Zulqarnain04 <147962196+Zulqarnain04@users.noreply.github.com>
2025-06-19 22:24:45 +0800

    Update db_connect.php

--- datebase/db_connect.php
+++ datebase/db_connect.php
@@ -1,1 +1,14 @@
+//connect to sql
 
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'accomodation_db';
+
+$conn = mysqli_connect($host, $user, %pass, $db);
+
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+?>
